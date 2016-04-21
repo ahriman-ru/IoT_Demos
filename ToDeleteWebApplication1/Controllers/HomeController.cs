ff087c7293162f0aad834c8216d6c9555c08d755
ahriman-ru <ahriman@ahriman.ru>
2016-04-21 18:49:28 +0300

    webapp to test github integrtaion with azure web app

---
+++ ToDeleteWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+
+namespace ToDeleteWebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+   CloudConfigurationManager.GetSetting("ahrimanstoraccount_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("vhds");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("ываыва.txt");
+            using (var fileStream = System.IO.File.OpenRead(@"c:\temp\jenkins.xml"))
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
